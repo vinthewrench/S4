368ab6172fe5a01e0eb5ee1c39c982beece45b91
vincent Moscaritolo <vinnie@vmeng.com>
2015-11-02 09:22:55 -0800

    Initial Commit

---
+++ C4Tests/C4Tests.m
@@ -0,0 +1,39 @@
+//
+//  C4Tests.m
+//  C4Tests
+//
+//  Created by vincent Moscaritolo on 11/2/15.
+//  Copyright © 2015 4th-A Technologies, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface C4Tests : XCTestCase
+
+@end
+
+@implementation C4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
