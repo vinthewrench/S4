05d2f31564646c36bdca0f95738c5203a245c253
Robbie Hanson <robbiehanson@deusty.com>
2017-10-25 17:08:04 -0400

    Adding iOS Framework target.

---
+++ S4-ios/S4_ios.h
@@ -0,0 +1,19 @@
+//
+//  S4_ios.h
+//  S4-ios
+//
+//  Created by Robbie Hanson on 10/15/17.
+//  Copyright © 2017 4th-A Technologies, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for S4_ios.
+FOUNDATION_EXPORT double S4_iosVersionNumber;
+
+//! Project version string for S4_ios.
+FOUNDATION_EXPORT const unsigned char S4_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <S4_ios/PublicHeader.h>
+
+
